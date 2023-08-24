79ce9eb6270b5b340e7447cdccfd82b7cdb97c2e
asinay <54133268+JanYanisa@users.noreply.github.com>
2023-08-24 18:41:07 +0700

    [update] 2.1: Course information step6

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,35 +5,45 @@ import Counter from "./tutorial-components/Counter"
 import LeftRightClick from "./tutorial-components/LeftRightClick"
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
-  console.log('App:rerender?')
-  return (<>
+  return (
+    <>
+      <Course course={course} />
+      <div className="tutorial">
+          <Counter/>
+          <LeftRightClick/>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+const Course = ({course}) => {
+  return (
     <div>
       <Header course = {course.name}/>
       <Content parts={course.parts}/>
       <Total parts={course.parts} />
     </div>
-    <div className="tutorial">
-      <Counter/>
-      <LeftRightClick/>
-    </div>
-  </>
   )
 }
-
-export default App
