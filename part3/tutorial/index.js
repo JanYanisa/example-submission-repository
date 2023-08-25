79ca541187bcc8dbc1eb026176fc5801abd52286
asinay <54133268+JanYanisa@users.noreply.github.com>
2023-08-25 23:26:08 +0700

    [update] part3-a : middleware

--- part3/tutorial/index.js
+++ part3/tutorial/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+app.use(requestLogger)
 
 let notes = [
     {
@@ -69,7 +77,11 @@ const generateId = () => {
     notes = notes.concat(note)
   
     response.json(note)
-  })
+})
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 const PORT = 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
