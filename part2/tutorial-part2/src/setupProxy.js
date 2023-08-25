7e21bc3a59031f5fc9702995d1647eb299252a08
asinay <54133268+JanYanisa@users.noreply.github.com>
2023-08-26 00:43:54 +0700

    [add] add proxy for frontend to still can be dev

---
+++ part2/tutorial-part2/src/setupProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function(app) {
+    app.use(createProxyMiddleware('/api', 
+        { target: 'http://localhost:3001/', changeOrigin: true,}
+    ));
+}
+//ref: https://stackoverflow.com/questions/52605997/when-specified-proxy-in-package-json-must-be-a-string
