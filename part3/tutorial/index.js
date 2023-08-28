4d78ff09ae106a60881b8abd805fc0759a8f89e9
asinay <54133268+JanYanisa@users.noreply.github.com>
2023-08-28 11:48:39 +0700

    [update] part3-c : Connecting the backend to a database

--- part3/tutorial/index.js
+++ part3/tutorial/index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const mongoose = require('mongoose')
 
+const url =
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.viy334d.mongodb.net/noteApp?retryWrites=true&w=majority`
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+const Note = mongoose.model('Note', noteSchema)
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 app.use(express.json())
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -35,7 +53,9 @@ let notes = [
 // })
 
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
