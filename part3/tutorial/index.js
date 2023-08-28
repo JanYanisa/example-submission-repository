21e05d007bac17df700f387ee0ee8f403355a409
asinay <54133268+JanYanisa@users.noreply.github.com>
2023-08-28 12:32:52 +0700

    [update] move connecting db to module and change to use module with error handling

--- part3/tutorial/index.js
+++ part3/tutorial/index.js
@@ -1,25 +1,9 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
 require('dotenv').config()
-const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const Note = require('./models/note')
 
-const url =
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.viy334d.mongodb.net/noteApp?retryWrites=true&w=majority`
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-const Note = mongoose.model('Note', noteSchema)
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+const app = express()
 app.use(express.json())
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -31,27 +15,6 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger)
 app.use(cors())
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-// app.get('/', (request, response) => {
-//     response.send('<h1>Hello World!</h1>')
-// })
-
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
@@ -59,55 +22,49 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = parseInt(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
         response.json(note)
-    } else {
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: `malformatted id, ${error.message}` })
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id).then(note => {
+      response.status(204).end()
+    })
 })
 
 app.put('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id === id ? request.body : note)
-  
-    response.json(request.body)
+    Note.findByIdAndUpdate(request.params.id, request.body ).then(note => {
+      response.json(request.body)
+    })
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
   
 app.post('/api/notes', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({ 
-        error: 'content missing' 
-        })
-    }
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
