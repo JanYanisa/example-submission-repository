67433078ce4ad608c2992ceae0d5b23627f8959c
asinay <54133268+JanYanisa@users.noreply.github.com>
2023-08-29 11:37:44 +0700

    [add] add users in collection db

--- part3/tutorial/app.js
+++ part3/tutorial/app.js
@@ -1,14 +1,17 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 // this has to be the last loaded middleware.
 app.use(middleware.errorHandler)
--- part3/tutorial/controllers/notes.js
+++ part3/tutorial/controllers/notes.js
@@ -1,14 +1,16 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+  Note.find({}).populate('user', { username: 1, name: 1 })
+    .then(notes => {
+      response.json(notes)
+    })
 })
 
 notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
+  Note.findById(request.params.id).populate('user', { username: 1, name: 1 })
     .then(note => {
       if (note) {
         response.json(note)
@@ -19,19 +21,20 @@ notesRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id
   })
 
-  note.save()
-    .then(savedNote => {
-      response.status(201).json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+  response.status(201).json(savedNote)
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
