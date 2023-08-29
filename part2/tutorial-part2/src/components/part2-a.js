7a7cb0d535c52d5285205c878f842bee299d8dc7
asinay <54133268+JanYanisa@users.noreply.github.com>
2023-08-29 16:50:59 +0700

    [add] part5-a: add Login in frontend

--- part2/tutorial-part2/src/components/part2-a.js
+++ part2/tutorial-part2/src/components/part2-a.js
@@ -1,6 +1,7 @@
 import Note from "./Note";
 import { useState, useEffect } from 'react'
 import notesService from '../services/notes.service'
+import loginService from '../services/login.service'
 
 const Part2a = () => {
   const initNewNote = 'a new note...'
@@ -9,6 +10,9 @@ const Part2a = () => {
   const [showAll, setShowAll] = useState(true)
   // const [errorMessage, setErrorMessage] = useState('some error happened...')
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   const hook = () => {
     notesService
@@ -18,6 +22,16 @@ const Part2a = () => {
       })
   }
   useEffect(hook, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      notesService.setToken(user.token)
+    }
+  }, [])
+
   if (!notes) { 
     return null 
   }
@@ -61,10 +75,46 @@ const Part2a = () => {
         setNotes(notes.filter(n => n.id !== id))
       })
   }
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      notesService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+  const logOut = () => {
+    window.localStorage.removeItem('loggedNoteappUser')
+    notesService.setToken(null)
+    setUser(null)
+  }
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {user === null ? 
+        <LoginForm handleLogin={handleLogin} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>
+        : <div>
+          <p>{user.name} logged in</p>
+          <button onClick={() => logOut()}>
+            log-out
+          </button>
+          <NoteForm addNote={addNote} newNote={newNote} handleNoteChange={handleNoteChange} initNewNote={initNewNote}/>
+        </div>
+        }
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -75,10 +125,6 @@ const Part2a = () => {
             <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} placeholder={initNewNote}/>
-        <button type="submit">save</button>
-      </form>  
       <Footer />
     </div>
   )
@@ -109,5 +155,44 @@ const Part2a = () => {
         <br />
         <em>Note app, Department of Computer Science, University of Helsinki 2023</em>
       </div>
+    )
+  }
+
+  const LoginForm = ({handleLogin, username, setUsername, password, setPassword}) => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>  
+    )    
+  }
+
+  const NoteForm = ({addNote, newNote, handleNoteChange, initNewNote}) => {
+    return (
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+          placeholder={initNewNote}
+        />
+        <button type="submit">save</button>
+      </form>
     )
   }
